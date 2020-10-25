fcad09ba5ab7ce054eb104a1016edaabf1ec18bc
SAMUEL HALE <sjhale@my.waketech.edu>
2020-10-24 21:17:15 -0400

    .

--- test.py
+++ test.py
@@ -3,19 +3,15 @@ import requests
 
 r = requests.get("http://api.duckduckgo.com/?q=presidents of the united states&format=json")
 
-list = ["George Washington","John Adams",
-"Thomas Jefferson","James Madison","James Monroe","John Quincy Adams","Andrew Jackson","Martin Van Buren",
-"Martin Van Buren", "William Henry Harrison","John Tyler","James K. Polk","Zachary Taylor","Millard Fillmore",
-"Millard Fillmore", "Franklin Pierce","James Buchanan","Abraham Lincoln","Andrew Johnson","Ulysses S. Grant",
-"Rutherford B. Hayes","James Garfield","Chester A. Arthur","Grover Cleveland","Benjamin Harrison","Grover Cleveland",
-"William McKinley","Theodore Roosevelt","William Howard Taft","Woodrow Wilson","Warren G. Harding","Calvin Coolidge",
-"Herbert Hoover","Franklin D. Roosevelt","Harry S. Truman","Dwight D. Eisenhower","John F. Kennedy","Lyndon B. Johnson",
-"Richard M. Nixon", "Gerald R. Ford", "James Carter","Ronald Reagan","George H. W. Bush","William J. Clinton","George W. Bush",
-"Barack Obama","Donald J. Trump"]
+RelatedTopics = ["Washington","Adams","Jefferson","Madison","Monroe","Adams","Jackson","Buren",
+"Harrison","Tyler","Polk","Taylor","Fillmore","Fillmore", "Pierce","Buchanan","Lincoln","Johnson","Grant",
+"Hayes","Garfield","Arthur","Cleveland","Harrison","Cleveland","McKinley","Roosevelt","Taft","Wilson","Harding","Coolidge",
+"Hoover","Roosevelt","Truman","Eisenhower","Kennedy","Johnson","Nixon", "Ford", "Carter","Reagan","Bush","Clinton","Bush"
+,"Obama","Trump"]
 
 
 def test_presidents():
-    assert (list in r.text) == True
+    assert RelatedTopics == r.text
         
     
     
